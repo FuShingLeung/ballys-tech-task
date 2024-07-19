e50e13956415f7c8c401659bb59f0bde2d3259cf
Fu Shing Leung <bobbyfs123@gmail.com>
2024-07-19 22:45:00 +0100

    implemented search by id in ui

--- public/js/main.js
+++ public/js/main.js
@@ -6,27 +6,56 @@ const form = document.querySelector('#search-repositories-form');
 const fetchRepositories = async (e) => {
   e.preventDefault();
   const formData = new FormData(form);
+
   const name = formData.get('name');
+  const id = formData.get('id');
 
-  const res = await fetch(`${LOCAL_ENDPOINT}/api/repositories?name=${name}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/vnd.github+json',
-    },
-  });
-
-  const data = await res.json();
-  const repositories = data.items;
-  output.innerHTML = '';
-
-  if (repositories.length > 0) {
-    repositories.forEach((repository) => {
-      const repositoryEl = document.createElement('div');
-      repositoryEl.textContent = repository.name;
-      output.appendChild(repositoryEl);
+  if (!name && !id) {
+    output.innerHTML = 'Please enter a search criteria';
+    return 'Please enter a search a criteria';
+  } else if (!id) {
+    const res = await fetch(`${LOCAL_ENDPOINT}/api/repositories?name=${name}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/vnd.github+json',
+      },
     });
-  } else {
-    output.innerHTML = `There are no repositories with the name ${name}`;
+
+    const data = await res.json();
+    const repositories = data.items;
+    output.innerHTML = '';
+
+    if (repositories.length > 0) {
+      repositories.forEach((repository) => {
+        const repositoryEl = document.createElement('div');
+        repositoryEl.textContent = repository.name;
+        output.appendChild(repositoryEl);
+      });
+    } else {
+      output.innerHTML = `There are no repositories with the name ${name}`;
+    }
+  } else if (!name) {
+    const res = await fetch(
+      `${LOCAL_ENDPOINT}/api/repositorydetails?id=${id}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/vnd.github+json',
+        },
+      },
+    );
+
+    const data = await res.json();
+    const repository = data.items;
+    output.innerHTML = '';
+
+    if (data.message === 'Not Found') {
+      output.innerHTML = `There are no repository with the id ${id}`;
+    } else {
+      const repositoryEl = document.createElement('div');
+      repositoryEl.textContent = data.name;
+      output.appendChild(repositoryEl);
+    }
   }
 };
 
